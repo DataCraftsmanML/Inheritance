c66ec37b395be166b497c41c68777002a155cb98
Jiya Savaliya <126229948+Jiyasavaliya@users.noreply.github.com>
2024-03-24 15:29:14 +0530

    Add files via upload

---
+++ multilevelinheritance.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+class Triangle {
+
+       public void Print(int num1, int num2){ 
+         System.out.println(num1+num2);
+       
+} 
+}
+
+class circle extends Triangle{
+
+
+    public void print(){ 
+        System.out.println("hy");
+} 
+}
+
+class square extends circle{
+
+    public void printif(){ 
+        System.out.println("hyyyyy");
+} 
+
+    
+}
+
+
+public class multilevelinheritance {
+
+public static void main(String[] args){
+
+  Account account1 = new Account();
+ account1.name = "jiya";     
+
+}
+}
